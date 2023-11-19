400acdc64d18e690a4837617913a5d3442c2616e
Bruce Matthew Kuwahara <bkuwahara@mail.cs.toronto.edu>
2023-11-19 18:58:48 -0500

    Created a script to be used for evaluating the model

---
+++ scripts/evaluate_model.py
@@ -0,0 +1,37 @@
+import json
+import os
+import random
+
+
+os.chdir("C:/Users/kuwah/Documents/Academics/U of T/CSC2542 Knowledge Representation/Final Project")
+
+with open("./data/task.json") as data_file:
+    data = json.load(data_file)
+
+prefix = data["task_prefix"]
+questions = data["examples"]
+
+def rand_model(query):
+    return "entailment" if random.randint(0,1) == 0 else "non-entailment"
+
+
+def llama_2_unassisted(input, k):
+    pass
+
+def llama_2_logical(input, k):
+    pass
+
+
+def evaluate(model):
+
+    model_score=0
+    for q in questions:
+        query = q["input"]
+        output = model(query)
+        score = q["target_scores"][output]
+        model_score += score
+
+    return model_score / len(questions)
+    
+
+
