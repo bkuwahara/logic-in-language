ef06bc2c9ad9930e4de9b735c3b31b8a9bac81ee
Bruce Matthew Kuwahara <bkuwahara@mail.cs.toronto.edu>
2023-11-20 18:42:28 -0500

    Added new scripts for inference with pipeline Added "your answer" to end of prompt for better generation Created virtual environment

--- scripts/evaluate_model.py
+++ scripts/evaluate_model.py
@@ -1,28 +1,28 @@
 import json
 import os
 import random
-
-
+import torch
+from transformers import LlamaForCausalLM, LlamaTokenizer
 os.chdir("/w/339/bkuwahara/csc2542")
 
-with open("./data/task.json") as data_file:
-    data = json.load(data_file)
+from llama_basic import load_llama
 
-prefix = data["task_prefix"]
-questions = data["examples"]
+def load_data():
+	with open("./data/task.json") as data_file:
+    		data = json.load(data_file)
+
+	prefix = data["task_prefix"]
+	questions = data["examples"]
+	
 
 def rand_model(query):
     return "entailment" if random.randint(0,1) == 0 else "non-entailment"
 
 
-def llama_2_unassisted(input, k):
-    pass
-
-def llama_2_logical(input, k):
-    pass
+model, tokenizer = load_llama()
 
 
-def evaluate(model):
+def evaluate(model, tokenizer):
 
     model_score=0
     for q in questions:
@@ -32,6 +32,6 @@ def evaluate(model):
         model_score += score
 
     return model_score / len(questions)
-    
+
 
 
--- scripts/test2.py
+++ scripts/test2.py
@@ -1,10 +1,36 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
-tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1", padding_side="left")
-model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", device_map="auto", load_in_4bit=True)
 
-model_inputs = tokenizer(["A list of colors: red, blue"], return_tensors="pt").to("cuda")
+def load_mistral():
+	tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1", padding_side="left")
+	tokenizer.pad_token = tokenizer.eos_token
+	model = AutoModelForCausalLM.from_pretrained("mistralai/Mistral-7B-v0.1", device_map="auto", load_in_4bit=True)
+	return model, tokenizer
 
 
-generated_ids = model.generate(**model_inputs, max_new_tokens=50)
-print(tokenizer.batch_decode(generated_ids, skip_special_tokens=True)[0])
+
+#model_inputs = tokenizer(["A list of colors: red, blue"], return_tensors="pt").to("cuda")
+
+
+if __name__ == "__main__":
+	import json
+	model, tokenizer = load_mistral()
+
+	test_prompt = "Hey, are you conscious? Can you talk to me?"
+	inputs = tokenizer(test_prompt, return_tensors="pt")
+	generate_ids = model.generate(**inputs, max_length=50)
+	print("test prompt output: " + tokenizer.batch_decode(generate_ids, skip_special_tokens=True)[0])
+
+
+	with open("../datasets/task.json") as data_file:
+		data = json.load(data_file)
+
+        #prefix = data["task_prefix"]
+	prefix = "Decide if the following premise logically entails the hypothesis. Respond with \"entailment\" or \"non-entailment\""
+	questions = data["examples"]
+	q = questions[0]
+	query = q["input"]
+	input = tokenizer(prefix + query, return_tensors="pt").to("cuda")
+	generate_ids = model.generate(**input, max_new_tokens=25)
+	output = tokenizer.batch_decode(generate_ids, skip_special_tokens=True)
+	print(output)
