dc7ce25712aea92b5a3215309b9ed43dc1aa6266
Bruce Matthew Kuwahara <bkuwahara@mail.cs.toronto.edu>
2023-12-09 10:08:49 -0500

    Modified translate prompts to allow the model to see both premise and hypothesis

--- scripts/models.py
+++ scripts/models.py
@@ -15,9 +15,10 @@ class LlamaBasic:
 	def __init__(self, model_path, chain_of_thought=False):
 
 		self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-		
+		offload_state_dict = torch.cuda.is_available()
+
 		tokenizer = AutoTokenizer.from_pretrained(model_path)
-		model = AutoModelForCausalLM.from_pretrained(model_path, device_map='auto',offload_folder='offload')
+		model = AutoModelForCausalLM.from_pretrained(model_path, device_map='auto',offload_folder='offload', offload_state_dict=offload_state_dict)
 		self.model = model
 		self.tokenizer = tokenizer
 
@@ -58,9 +59,10 @@ class LlamaLogical:
 	# model: string specifying the model to use, e.g. "13b"
 	def __init__(self, model_path, chain_of_thought=False):		
 		self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+		offload_state_dict = torch.cuda.is_available()
 		
 		tokenizer = AutoTokenizer.from_pretrained(model_path)
-		model = AutoModelForCausalLM.from_pretrained(model_path, device_map='auto',offload_folder='offload')
+		model = AutoModelForCausalLM.from_pretrained(model_path, device_map='auto',offload_folder='offload', offload_state_dict=offload_state_dict)
 		self.model = model
 		self.tokenizer = tokenizer
 		self.chain_of_thought=chain_of_thought
@@ -84,9 +86,8 @@ class LlamaLogical:
 	# Prompts the model to convert a statement into epistemic logic in string form
 	# statement: string to convert into logic
 	def convert_to_logic(self, statement, max_new_tokens=100, return_full_output=False):
-		input = self.tokenizer(self.prompt+'\n'+"Premise: "+statement, return_tensors="pt").to(self.device)
+		input = self.tokenizer(self.prompt+'\n'+statement, return_tensors="pt").to(self.device)
 		generate_ids = self.model.generate(**input, max_new_tokens=max_new_tokens, past_key_values=self.encoded_prompt)
-		
 		
 		model_output = self.tokenizer.batch_decode(generate_ids, skip_special_tokens=True)[0]
 
@@ -99,20 +100,35 @@ class LlamaLogical:
 
 	# Gets the model's prediction for a given problem
 	# task: string specifying the inference task
-	def __call__(self, task):
-		max_new_tokens = 100 if self.chain_of_thought else 20
-		prem, hyp = task.split("Premise: ", maxsplit=1)[1].split("Hypothesis: ")
-		print("Premise string: " + prem)
-		print("Hypothesis string: " + hyp)
-		premise_logic = self.convert_to_logic(prem, max_new_tokens=max_new_tokens)
-		hypothesis_logic = self.convert_to_logic(hyp, max_new_tokens=max_new_tokens)
-		print("premise logic: " + premise_logic)
-		print("hypothesis logic: " + hypothesis_logic)
-		KB_prem = KnowledgeBase.from_string(premise_logic)
-		KB_hyp = KnowledgeBase.from_string(hypothesis_logic)
-		
-		answer = KB_prem.entails(KB_hyp)
-		return "entailment" if answer else "non-entailment"
+	def __call__(self, task, return_full_output=False, max_new_tokens=100):
+		input = self.tokenizer(self.prompt+'\n'+task, return_tensors="pt").to(self.device)
+		generate_ids = self.model.generate(**input, max_new_tokens=max_new_tokens, past_key_values=self.encoded_prompt)
+		model_output = self.tokenizer.batch_decode(generate_ids, skip_special_tokens=True)[0]
+
+		if return_full_output:
+			return model_output
+		else:
+			_p, _h = out.split("Translated premise: ")[-1].split("Translated hypothesis: ")
+			premise_logic = _p.split("$$")[1]
+			hypothesis_logic = _h.split("$$")[1]
+			try:
+				KB_prem = KnowledgeBase.from_string(premise_logic)
+				KB_hyp = KnowledgeBase.from_string(hypothesis_logic)
+				answer = KB_prem.entails(KB_hyp)
+				return "entailment" if answer else "non-entailment"
+			except:
+				return "Invalid response"
+
+
+
+#		prem, hyp = task.split("Premise: ", maxsplit=1)[1].split("Hypothesis: ")
+#		print("Premise string: " + prem)
+#		print("Hypothesis string: " + hyp)
+#		premise_logic = self.convert_to_logic(prem, max_new_tokens=max_new_tokens)
+#		hypothesis_logic = self.convert_to_logic(hyp, max_new_tokens=max_new_tokens)
+#		print("premise logic: " + premise_logic)
+#		print("hypothesis logic: " + hypothesis_logic)
+#
 
 
 # For testing the rest of the code quickly
@@ -135,9 +151,9 @@ if __name__ == "__main__":
 	import json
 
 #	model = LlamaBasic("meta-llama/Llama-2-13b-hf")
-	model = LlamaLogical("meta-llama/Llama-2-13b-hf")
+#	model = LlamaLogical("meta-llama/Llama-2-13b-hf")
 #	model = LlamaBasic("meta-llama/Llama-2-13b-hf", chain_of_thought=True)
-#	model = LlamaLogical("meta-llama/Llama-2-13b-hf", chain_of_thought=True)
+	model = LlamaLogical("meta-llama/Llama-2-13b-hf", chain_of_thought=True)
 
 #	model = LlamaLogical("meta-llama/Llama-2-13b-hf")
 
@@ -146,7 +162,7 @@ if __name__ == "__main__":
 		data = json.load(data_file)
 
 	#prefix = data["task_prefix"]
-	questions = random.choices(data["examples"], k=10)
+	questions = random.choices(data["examples"], k=2)
 	
 	for question in questions:
 		q = question["input"]
@@ -156,5 +172,5 @@ if __name__ == "__main__":
 	#print("Full model output: "+out)
 	#logic_string = out.split("Answer: ")[-1].split("$$")[1]
 	#print("Isolated logic string: "+logic_string)
-		answer = model(q)
+		answer = model(q, return_full_output=True, max_new_tokens=100)
 		print(answer)
